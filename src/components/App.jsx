5412d8ae6eb5fd737a65cd6c436d5a364cd2fb16
Pavlo <novakpasha13@gmail.com>
2025-04-15 19:59:42 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import AboutSection from './AboutSection/AboutSection';
 import CategorySection from './CategorySection/CategorySection';
@@ -13,20 +13,28 @@ import SponsorsSection from './SponsorsSection/Sponsors';
 import TestimonialsSection from './TestimonialsSection/TestimonialsSection';
 
 function App() {
+  const [activeDay, setActiveDay] = useState(1); // Состояние для активного дня
+
+  const days = [
+    { day: 'DAY 01', date: '16 AUGUST' },
+    { day: 'DAY 02', date: '17 AUGUST' },
+    { day: 'DAY 03', date: '18 AUGUST' },
+  ];
+
   return (
     <div className="App">
-    <Header />
-    <CategorySection />
-    <AboutSection />
-    <SingersSection />
-    <ScheduleSection />
-    <PriceSection />
-    <SponsorsSection />
-    <TestimonialsSection />
-    <GallerySection />
-    <ContactSection />
-    <Footer />
-  </div>
+      <Header />
+      <CategorySection />
+      <AboutSection />
+      <SingersSection />
+      <ScheduleSection days={days} activeDay={activeDay} setActiveDay={setActiveDay} />
+      <PriceSection />
+      <SponsorsSection />
+      <TestimonialsSection />
+      <GallerySection />
+      <ContactSection />
+      <Footer />
+    </div>
   );
 }
 
